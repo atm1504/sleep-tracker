43571dfc4d6a26645cec0c9f74c0fb9aaa82fe43
Amartya Mondal <atm1504.in@gmail.com>
2021-08-02 22:17:54 +0530

    Update

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightViewHolder.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightViewHolder.kt
@@ -4,9 +4,6 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
-import com.example.android.trackmysleepquality.R
-import com.example.android.trackmysleepquality.convertDurationToFormatted
-import com.example.android.trackmysleepquality.convertNumericQualityToString
 import com.example.android.trackmysleepquality.database.SleepNight
 import com.example.android.trackmysleepquality.databinding.ListItemSleepNightBinding
 
@@ -14,22 +11,24 @@ class SleepNightViewHolder private constructor(val binding: ListItemSleepNightBi
     RecyclerView.ViewHolder(binding.root) {
 
     fun bind(item: SleepNight) {
-        val res = itemView.context.resources
-
-        binding.sleepLength.text =
-            convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res)
-        binding.qualityString.text = convertNumericQualityToString(item.sleepQuality, res)
-        binding.qualityImage.setImageResource(
-            when (item.sleepQuality) {
-                0 -> R.drawable.ic_sleep_0
-                1 -> R.drawable.ic_sleep_1
-                2 -> R.drawable.ic_sleep_2
-                3 -> R.drawable.ic_sleep_3
-                4 -> R.drawable.ic_sleep_4
-                5 -> R.drawable.ic_sleep_5
-                else -> R.drawable.ic_launcher_sleep_tracker_foreground
-            }
-        )
+        binding.sleep=item
+        binding.executePendingBindings()
+//        val res = itemView.context.resources
+//
+//        binding.sleepLength.text =
+//            convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res)
+//        binding.qualityString.text = convertNumericQualityToString(item.sleepQuality, res)
+//        binding.qualityImage.setImageResource(
+//            when (item.sleepQuality) {
+//                0 -> R.drawable.ic_sleep_0
+//                1 -> R.drawable.ic_sleep_1
+//                2 -> R.drawable.ic_sleep_2
+//                3 -> R.drawable.ic_sleep_3
+//                4 -> R.drawable.ic_sleep_4
+//                5 -> R.drawable.ic_sleep_5
+//                else -> R.drawable.ic_launcher_sleep_tracker_foreground
+//            }
+//        )
     }
 
     companion object {
